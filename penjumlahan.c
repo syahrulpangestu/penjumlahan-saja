aaf7ff88149afa327da8d2b556ee0d79307fa4ad
syahrulpangestu <syahrulpangestu11@gmail.com>
2021-08-09 00:34:38 +0700

    first commit

---
+++ penjumlahan.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int angka1;
+    int angka2;
+    int hasil;
+
+    printf("Masukan angka 1 : ");
+    scanf("%d", &angka1);
+    printf("Masukan angka 2 : ");
+    scanf("%d", &angka2);
+
+    hasil = angka1 + angka2;
+    printf("Hasil penjumlahan = %d", hasil);
+}
